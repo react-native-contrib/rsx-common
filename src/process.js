0a13843f45ceca41f3c193f44c92c9abb11a921c
Hassan Khan <contact@hassankhan.me>
2016-05-07 14:35:30 +0100

    Moved `makeCommand` into `process.run`

--- src/process.js
+++ src/process.js
@@ -1,13 +1,13 @@
 const spawn = require('child_process').spawn;
 
-module.exports = function makeCommand(command) {
-  return (cb) => {
-    if (!cb) {
+const run = (command, callback) => {
+  return (callback) => {
+    if (!callback) {
       throw new Error(`You missed a callback function for the ${command} command`);
     }
 
     const args = command.split(' ');
-    const cmd = args.shift();
+    const cmd  = args.shift();
 
     const commandProcess = spawn(cmd, args, {
       stdio: 'inherit',
@@ -16,10 +16,14 @@ module.exports = function makeCommand(command) {
 
     commandProcess.on('close', function prelink(code) {
       if (code) {
-        throw new Error(`Error occured during executing "${command}" command`);
+        throw new Error(`Error occured while executing "${command}" command`);
       }
 
-      cb();
+      callback();
     });
   };
+};
+
+module.exports = {
+    run: run,
 };
