a46eb62946d290b0f63ea8cb921ad1f3556d17af
Hassan Khan <contact@hassankhan.me>
2016-05-09 23:29:59 +0100

    Adding test cases for `project` and `validate`

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -1,4 +1,6 @@
 const chai = require('chai');
+const mock = require('mock-require');
+const sinon = require('sinon');
 const path = require('path');
 const projectUtils = require('../src/project');
 
@@ -7,7 +9,19 @@ const expect = chai.expect;
 describe('project', () => {
 
     describe('#getPackageJson', () => {
-        it('should return the package file as a JSON object');
+
+        afterEach(() => {
+            mock.stopAll();
+        });
+
+        it('should return the package file as a JSON object', () => {
+            mock(path.join(process.cwd(), 'package.json'), {
+                name: 'Test Package'
+            });
+
+            expect(projectUtils.getPackageJson(process.cwd()).name).to.deep.equals('Test Package');
+        });
+
         it('should return an empty array');
     });
 
--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -7,13 +7,24 @@ const expect = chai.expect;
 describe('validate', () => {
 
     describe('#isPackageName', () => {
-        it('should return true for a valid package name');
+
+        it('should return true for a valid package name', () => {
+            expect(validateUtils.isPackageName('com.rsx.app')).to.deep.equals(true);
+        });
+
         it('should return false for an invalid package name');
     });
 
     describe('#isPlugin', () => {
-        it('should return true for a valid RSX plugin');
-        it('should return false for an invalid RSX plugin');
+
+        it('should return true for a valid React Native plugin', () => {
+            expect(validateUtils.isPlugin('react-native-test')).to.deep.equals(true);
+        });
+
+        it('should return false for anything else', () => {
+            expect(validateUtils.isPlugin('rxs-plugin-test')).to.deep.equals(false);
+        });
+
     });
 
 });
