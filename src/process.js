2a66ad1c595ae957743dc4392768a223ed2840d1
Hassan Khan <contact@hassankhan.me>
2016-05-15 02:10:05 +0100

    Allow `spawnArgs` to be passed as a second argument to `process.run()`

--- src/process.js
+++ src/process.js
@@ -1,6 +1,8 @@
 const spawn = require('child_process').spawn;
 
-const run = (command) => {
+const run = (command, sArgs) => {
+    const spawnArgs = sArgs || {stdio: 'inherit', stdin: 'inherit'};
+
     return (callback) => {
         if (!callback) {
             throw new Error(`You missed a callback function for the ${command} command`);
@@ -9,10 +11,7 @@ const run = (command) => {
         const args = command.split(' ');
         const cmd  = args.shift();
 
-        const commandProcess = spawn(cmd, args, {
-            stdio: 'inherit',
-            stdin: 'inherit',
-        });
+        const commandProcess = spawn(cmd, args, spawnArgs);
 
         commandProcess.on('close', function prelink(code) {
             if (code) {
